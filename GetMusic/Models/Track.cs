cac10b29ab329f3e38396e94e8a2a8c5a349f797
Dor Weiser <200876704@stud.colman.ac.il>
2017-09-22 13:15:51 +0300

    first upload

---
+++ GetMusic/Models/Track.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GetMusic.Models
+{
+    public class Track
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Minute { get; set; }
+        public int Second { get; set; }
+        public Album Album { get; set; }
+    }
+}
