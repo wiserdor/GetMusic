7c846bf304410be2d498ba0625b29a901d929603
Dor Weiser <200876704@stud.colman.ac.il>
2017-09-22 18:43:33 +0300

    login register

--- GetMusic/Controllers/UsersController.cs
+++ GetMusic/Controllers/UsersController.cs
@@ -7,6 +7,8 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using GetMusic.Models;
+using System.Data.Entity.Validation;
+using System.Web.Security;
 
 namespace GetMusic.Controllers
 {
@@ -19,7 +21,95 @@ namespace GetMusic.Controllers
         {
             return View(db.MyUsers.ToList());
         }
+        [HttpGet]
+        public ActionResult LogIn()
+        {
+            return View();
+        }
 
+        [HttpPost]
+        public ActionResult LogIn(Models.User user)
+        {
+            //if (ModelState.IsValid)  
+            //{  
+            if (IsValid(user.Name, user.Password))
+            {
+                FormsAuthentication.SetAuthCookie(user.Name, false);
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ModelState.AddModelError("", "Login details are wrong.");
+            }
+            return View(user);
+        }
+
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(Models.User user)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                        var newUser = db.MyUsers.Create();
+                        newUser.Name = user.Name;
+                        newUser.Password = user.Password;
+                        newUser.UserType = "User";
+                        db.MyUsers.Add(newUser);
+                        db.SaveChanges();
+                        return RedirectToAction("Index", "Home");
+                    
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Data is not correct");
+                }
+            }
+            catch (DbEntityValidationException e)
+            {
+                foreach (var eve in e.EntityValidationErrors)
+                {
+                    Console.WriteLine("Entity of type \"{0}\" in state \"{1}\" has the following validation errors:",
+                        eve.Entry.Entity.GetType().Name, eve.Entry.State);
+                    foreach (var ve in eve.ValidationErrors)
+                    {
+                        Console.WriteLine("- Property: \"{0}\", Error: \"{1}\"",
+                            ve.PropertyName, ve.ErrorMessage);
+                    }
+                }
+                throw;
+            }
+            return View();
+        }
+
+        public ActionResult LogOut()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Home");
+        }
+
+        private bool IsValid(string name, string password)
+        {
+            bool IsValid = false;
+
+    
+                var user = db.MyUsers.ToList().Find(u => u.Name == name);
+                if (user != null)
+                {
+                    if (user.Password == password)
+                    {
+                        IsValid = true;
+                    }
+                }
+            
+            return IsValid;
+        }
         // GET: Users/Details/5
         public ActionResult Details(int? id)
         {
--- GetMusic/Models/User.cs
+++ GetMusic/Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,8 +9,15 @@ namespace GetMusic.Models
     public class User
     {
         public int Id { get; set; }
+        [Required]
+        [Display(Name = "User Name: ")]
         public string Name { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(150, MinimumLength = 3)]
+        [Display(Name = "Password: ")]
         public string Password { get; set; }
+        public string UserType { get; set; }
         public IEnumerable<Album> Purchases { get; set; }
 
     }
